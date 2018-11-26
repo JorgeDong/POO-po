9f1c0082f5b46b3226a09bb47cd7131bf6806d28
ricardodf <39528804+ricardodf@users.noreply.github.com>
2018-11-26 10:07:44 -0600

    Update (26/10/18)

--- TSP-SA-Version1.0/CityRecorder.java
+++ TSP-SA-Version1.0/CityRecorder.java
@@ -1,86 +1,60 @@
-/*
-      CityRecorder.java - Version 1.0 (02/11/18)
-*/
-
-package TSP_V1;
 
 import java.util.ArrayList;
 
-/*
-    CityRecorder: Es una clase que guarda todas las ciudades en un mapa,
-        La idea es que aqui es el lugar donde se "guardará" todas las ciudades que se necesiten
-        y que cada algoritmo use esta clase y sus metodos
-    Contiene:
-    -> ArrayList allCities: Guarda todas las ciudades
-    -> City baseCity: Guarda la ciudad base donde se empieza a calcular las rutas
- */
 public class CityRecorder{
     // Atributos
-    protected ArrayList allCities;
+    private ArrayList cities;
     protected City baseCity;
 
     // Constructor
     public CityRecorder(){
         this.baseCity = null;
-        this.allCities = new ArrayList<City>();
-    }
-
-    //Declaración de la ciudad base
-    public void declareBaseCity(City c){
-        // SI no se ha asignado una ciudad base, se declara igualmente
-        if(this.baseCity == null){
-            this.baseCity = c;
-            for(int i=0; i<this.numOfCities(); i++){
-                if(this.getCity(i) == c) {
-                    this.allCities.remove(i);
-                    break;
-                }
-            }
-        }
-        // SI ya ha sido declarada antes, nos aseguramos de guardar la ciudad base y
-        // ponerlo en la lista con las demás ciudades
-        else {
-            City temp = this.baseCity;
-            this.allCities.add(temp);
-            this.baseCity = c;
-            for(int i=0; i<this.numOfCities(); i++){
-                if(this.getCity(i) == c) {
-                    this.allCities.remove(i);
-                    break;
-                }
-            }
-        }
+        this.cities = new ArrayList<City>();
     }
 
     public City getBaseCity(){ return this.baseCity; }
-    // Devuelve la lista completa de ciudades guardades en este CityRecorder
-      
-    public ArrayList getRecorder() {
-        return allCities;
+    public ArrayList getCities() {
+        return cities;
+    }
+    public int getCitiesSize(){
+        return cities.size();
     }
 
-    // Añade una ciudad, siempre al final
-    public void addCity(City city) {
-        allCities.add(city);
-    }
-
-    //Devuelve la ciudad en la posición 'id'
     public City getCity(int ind){
-        return (City)allCities.get(ind);
+        return (City)cities.get(ind);
+    }
+    public void addCity(City city) {
+        if (city != baseCity)
+            cities.add(city);
     }
 
-    //Devuelve el total de ciudades dentro de la lista
-    public int numOfCities(){
-        return allCities.size();
+    public void setBaseCity(City c){
+        if(this.baseCity == null){
+            this.baseCity = c;
+            for(int i=0; i<this.getCitiesSize(); i++){
+                if(this.getCity(i) == c) {
+                    getCities().remove(i);
+                    break;
+                }
+            }
+        } else {
+            City temp = this.baseCity;
+            getCities().add(temp);
+            this.baseCity = c;
+            for(int i=0; i<this.getCitiesSize(); i++){
+                if(this.getCity(i) == c) {
+                    getCities().remove(i);
+                    break;
+                }
+            }
+        }
     }
 
-    //Imprime todas las ciudades (en el orden en que se fueron agregando)
-    // [!] - Necesita mejora, dar una mejor formato
-    public void printAllCities(){
+    public void printRecorder(){
         System.out.println("Base City: " + this.baseCity);
         String output = "";
-        for(Object c : allCities){
-            output = "ID: "+allCities.indexOf(c)+" - "+c.toString();
+        for(Object c : cities){
+            output = "ID: "+cities.indexOf(c)+" - "+c.toString();
             System.out.println(output);
         }
     }
